56cadc34a334e3edfe406f1f8e061ca15c83dc70
torontotong <32303160+torontotong@users.noreply.github.com>
2019-02-02 18:27:54 -0500

    Mobile app dev (#26)
    * Checkin baseline MobileApp project into mobileapp branch.
    
    See details @ issue #11 and #12
    
    * Create Readme.md

---
+++ MobileApplication/app/src/androidTest/java/ca/uw/ece/mobileapplication/LoginActivityTest.java
@@ -0,0 +1,49 @@
+package ca.uw.ece.mobileapplication;
+
+import static org.junit.Assert.*;
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+
+import ca.uw.ece.mobileapplication.LoginActivity;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+
+import static android.support.test.espresso.matcher.ViewMatchers.isClickable;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static org.junit.Assert.assertEquals;
+
+@RunWith(AndroidJUnit4.class)
+public class LoginActivityTest {
+
+    @Rule
+    public ActivityTestRule<LoginActivity> mActivityRule = new ActivityTestRule<>(LoginActivity.class);
+
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("ca.uw.ece.mobileapplication", appContext.getPackageName());
+    }
+
+    @Test
+    public void user_login() throws InterruptedException {
+        onView(withId(R.id.userName)).perform(typeText("Hello"));
+        onView(withId(R.id.password)).perform(typeText("World"));
+        onView(withId(R.id.systolic)).perform(typeText("120"));
+        onView(withId(R.id.diastolic)).perform(typeText("75"));
+        onView(withId(R.id.sign_in_button)).perform(click()).check(matches(isClickable()));
+    }
+
+
+}
