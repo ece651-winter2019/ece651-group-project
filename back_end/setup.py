2fd0fd5ad5edfb7646f9a9b3481639836ad28fc5
Adam Kulas <33091324+adkulas@users.noreply.github.com>
2019-02-03 21:30:40 -0600

    Backend "hello world" application (#25)
    * basic back end using pyramid cookiecutter and sqlite db
    
    * update readme
    
    * changed file extension
    
    * Update MobileAppUserStory.md (#24)
    
    Remove Quality Requirement for now.
    
    * corrected problem with getting started commands
    
    Some people were having issues with the \n not print a newline character with echo. The \n has been removed
    
    * Mobile app dev (#26)
    
    * Checkin baseline MobileApp project into mobileapp branch.
    
    See details @ issue #11 and #12
    
    * Create Readme.md
    
    * Update .gitignore (#30)
    
    The .DS_Store files are giving me some troubles when I I try to commit or switch branches. 
    I added ".DS_Store" to solve this issue, so please someone review it ASAP.
    
    * Database (#31)
    
    * Database branch
    
    * Database ER: 2nd draft
    
    * updated
    
    * updated ER diagram and the sql file
    
    * adding schema.sql and ER Diagram
    
    * removed DS_Store file
    
    * basic back end using pyramid cookiecutter and sqlite db
    
    * update readme
    
    * changed file extension

---
+++ back_end/setup.py
@@ -0,0 +1,61 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'README.md')) as f:
+    README = f.read()
+with open(os.path.join(here, 'CHANGES.txt')) as f:
+    CHANGES = f.read()
+
+requires = [
+    'plaster_pastedeploy',
+    'pyramid',
+    'pyramid_jinja2',
+    'pyramid_debugtoolbar',
+    'waitress',
+    'alembic',
+    'pyramid_retry',
+    'pyramid_tm',
+    'SQLAlchemy',
+    'transaction',
+    'zope.sqlalchemy',
+]
+
+tests_require = [
+    'WebTest >= 1.3.1',  # py3 compat
+    'pytest >= 3.7.4',
+    'pytest-cov',
+]
+
+setup(
+    name='back_end',
+    version='0.0',
+    description='back_end',
+    long_description=README + '\n\n' + CHANGES,
+    classifiers=[
+        'Programming Language :: Python',
+        'Framework :: Pyramid',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: WSGI :: Application',
+    ],
+    author='',
+    author_email='',
+    url='',
+    keywords='web pyramid pylons',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    extras_require={
+        'testing': tests_require,
+    },
+    install_requires=requires,
+    entry_points={
+        'paste.app_factory': [
+            'main = back_end:main',
+        ],
+        'console_scripts': [
+            'initialize_back_end_db=back_end.scripts.initialize_db:main',
+        ],
+    },
+)
