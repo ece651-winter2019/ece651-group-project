9da0d2e218bcbeb8c02accaac24bc394513b2b20
Ammar Ahmed <ammarahmed@v1020-wn-9-5.campus-dynamic.uwaterloo.ca>
2019-02-19 04:07:07 -0500

    added python files for signing up both doctors and patients

---
+++ Database/src/doctor_signup.py
@@ -0,0 +1,33 @@
+
+from sqlalchemy.ext.declarative import declarative_base
+from datetime import datetime
+from sqlalchemy.orm import mapper, sessionmaker, relationship
+from sqlalchemy import *
+from sqlalchemy.dialects.mysql import LONGTEXT
+from databasemodel import Doctor
+
+
+engine = create_engine('mysql+mysqldb://bd3121794ba1e4:6d936518@us-cdbr-iron-east-03.cleardb.net/heroku_e0771598287fecc')
+connection = engine.connect()
+
+
+
+Session = sessionmaker(engine)
+session = Session()
+
+
+doc = Doctor(doctor_id = 4,
+             first_name = "worst",
+             last_name = "doctor",
+             email = "worst@gmail.com",
+             phone_no = 4389791679,
+             st = "32 king st s",
+             city = "waterloo",
+             state= "ON",
+             postal_code = "N3L9f3",
+            country = "Canada")
+
+session.add(doc)
+session.commit()
+session.close()
+
