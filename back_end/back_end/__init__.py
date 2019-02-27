6d7b01cbdb6b21cfdcfa8db398c5cba921da7b31
Adam Kulas <adam.kulas1@gmail.com>
2019-02-26 22:08:39 -0500

    changes to allow heroku to run

--- back_end/back_end/__init__.py
+++ back_end/back_end/__init__.py
@@ -1,12 +1,16 @@
 from pyramid.config import Configurator
-
+import os
 
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
+    if os.environ.get('DATABASE_URL', ''):
+        settings['sqlalchemy.url'] = os.environ['DATABASE_URL', '']
+
     with Configurator(settings=settings) as config:
         config.include('.models')
         config.include('pyramid_jinja2')
         config.include('.routes')
         config.scan()
+
     return config.make_wsgi_app()
