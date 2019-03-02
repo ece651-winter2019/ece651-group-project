306a5c4f3b0c8b4e2b1734a9d9922c41befabdc8
Adam Kulas <33091324+adkulas@users.noreply.github.com>
2019-03-02 13:11:23 -0500

    Deployment - switched to django framework (#55)
    * changes to allow heroku to run
    
    * changes
    
    * db connection made
    
    * can query doctors table from heroku database
    
    * add mysql connector
    
    * changes
    
    * changes
    
    * changes
    
    * changes
    
    * changes
    
    * database finally connects
    
    * changes
    
    * changed mysql driver to connector-python
    
    * changes
    
    * trying myselg-connector-python engine
    
    * allow migrations
    
    * added models for patients and doctors
    
    * disabling backend tests for now
    
    * added example test
    
    * run basic test
    
    * changes
    
    * changes

--- back_end/back_end/views/default.py
+++ back_end/back_end/views/default.py
@@ -5,28 +5,22 @@ from sqlalchemy.exc import DBAPIError
 
 from .. import models
 
+# @view_config(route_name='home', renderer='../templates/mytemplate.jinja2')
+# def my_view(request):
+#     # try:
+#     #     query = request.dbsession.query(models.MyModel)
+#     #     one = query.filter(models.MyModel.name == 'one').first()
+#     # except DBAPIError:
+#     #     return Response(db_err_msg, content_type='text/plain', status=500)
+#     print(request.dbsession.execute('SELECT * FROM doctors').fetchall())
+#     return {'one': 'ONE', 'project': type(request.dbsession)}
 
-@view_config(route_name='home', renderer='../templates/mytemplate.jinja2')
+@view_config(route_name='home', renderer=None)
 def my_view(request):
-    try:
-        query = request.dbsession.query(models.MyModel)
-        one = query.filter(models.MyModel.name == 'one').first()
-    except DBAPIError:
-        return Response(db_err_msg, content_type='text/plain', status=500)
-    return {'one': one, 'project': 'back_end'}
-
-
-db_err_msg = """\
-Pyramid is having a problem using your SQL database.  The problem
-might be caused by one of the following things:
-
-1.  You may need to initialize your database tables with `alembic`.
-    Check your README.txt for descriptions and try to run it.
-
-2.  Your database server may not be running.  Check that the
-    database server referred to by the "sqlalchemy.url" setting in
-    your "development.ini" file is running.
-
-After you fix the problem, please restart the Pyramid application to
-try it again.
-"""
+    # try:
+    #     query = request.dbsession.query(models.MyModel)
+    #     one = query.filter(models.MyModel.name == 'one').first()
+    # except DBAPIError:
+    #     return Response(db_err_msg, content_type='text/plain', status=500)
+    result = request.dbsession.execute('SELECT * FROM doctors').fetchall()
+    return Response(str(result))
---
+++ healthapp_project/healthapp_root/doctors/tests.py
@@ -0,0 +1,8 @@
+from django.test import TestCase
+import pytest
+
+def func(x):
+    return x + 1
+
+def test_answer():
+    assert func(3) == 4
