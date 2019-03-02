4c492889237922500383b25e89ed674500822323
Adam Kulas <adam.kulas1@gmail.com>
2019-03-02 13:39:38 -0500

    add user app for custom user model

---
+++ healthapp_project/healthapp_root/users/urls.py
@@ -0,0 +1,7 @@
+# users/urls.py
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('signup/', views.SignUp.as_view(), name='signup'),
+]
--- healthapp_project/healthapp_root/users/views.py
+++ healthapp_project/healthapp_root/users/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+# users/views.py
+from django.urls import reverse_lazy
+from django.views import generic
 
-# Create your views here.
+from .forms import CustomUserCreationForm
+
+class SignUp(generic.CreateView):
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'signup.html'
