d09198718cb83059213258e7ac203b63b2231add
Adam Kulas <33091324+adkulas@users.noreply.github.com>
2019-03-02 19:52:29 -0500

    User logins (#66)
    * changes to allow heroku to run
    
    * changes
    
    * db connection made
    
    * can query doctors table from heroku database
    
    * add mysql connector
    
    * changes
    
    * changes
    
    * changes
    
    * changes
    
    * changes
    
    * database finally connects
    
    * changes
    
    * changed mysql driver to connector-python
    
    * changes
    
    * trying myselg-connector-python engine
    
    * allow migrations
    
    * added models for patients and doctors
    
    * disabling backend tests for now
    
    * added example test
    
    * run basic test
    
    * changes
    
    * changes
    
    * add login logout and signup pages
    
    * changes
    
    * add email  backend
    
    * add password reset functionality
    
    * deleted database and remigrated to have custom user models
    
    * add user app for custom user model
    
    * removed unsed accounts app - orig created from tutorial
    
    * removed toy app

--- healthapp_project/healthapp_root/healthapp_site/settings.py
+++ healthapp_project/healthapp_root/healthapp_site/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
@@ -57,7 +58,7 @@ ROOT_URLCONF = 'healthapp_site.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,6 +122,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
+
+EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
+AUTH_USER_MODEL = 'users.CustomUser'
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
