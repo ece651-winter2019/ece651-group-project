d46d63607b266778b76646bed009fd60144148d8
Gabe Richard <gabe_rch@hotmail.com>
2019-03-09 23:32:51 -0500

    changed routes

--- healthapp_project/healthapp_root/api/urls.py
+++ healthapp_project/healthapp_root/api/urls.py
@@ -4,6 +4,6 @@ from api import views
 urlpatterns = [
     path('api/', views.api_list),
     path('api/<int:pk>/', views.api_detail),
-    path('api/patientrecords', views.api_list),
-    path('api/patientrecords/<int:pk>/', views.api_detail),
+    path('api/patientrecords', views.all_patient_records),
+    path('api/patientrecords/<int:pk>/', views.patient_record),
 ]
