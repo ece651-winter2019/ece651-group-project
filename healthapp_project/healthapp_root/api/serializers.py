9f946e4d862d120681752962226f7ee9012d2ed8
Gabe Richard <gabe_rch@hotmail.com>
2019-03-09 22:22:51 -0500

    destroyed database and remade it :)

--- healthapp_project/healthapp_root/api/serializers.py
+++ healthapp_project/healthapp_root/api/serializers.py
@@ -1,8 +1,15 @@
 from rest_framework import serializers
 from api.models import Api, LANGUAGE_CHOICES, STYLE_CHOICES
-
+from patients.models import PatientRecord
 
 class ApiSerializer(serializers.ModelSerializer):
     class Meta:
         model = Api
         fields = ('id', 'title', 'code', 'linenos', 'language', 'style')
+
+class RecordsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PatientRecord
+        fields = ('record_id', 'patient_id', 'bp_systolic', 'bp_diastolic', 'heart_rate', 'weight', 'created_on')
+
+    
--- healthapp_project/healthapp_root/api/views.py
+++ healthapp_project/healthapp_root/api/views.py
@@ -5,11 +5,13 @@ from rest_framework.renderers import JSONRenderer
 from rest_framework.parsers import JSONParser
 from api.models import Api
 from api.serializers import ApiSerializer
+from api.serializers import RecordsSerializer
+from patients.models import PatientRecord
 
 @csrf_exempt
 def api_list(request):
     """
-    List all code api's, or create a new api.
+    List all code api's, or create a new __.
     """
     if request.method == 'GET':
         api = Api.objects.all()
@@ -27,7 +29,7 @@ def api_list(request):
 @csrf_exempt
 def api_detail(request, pk):
     """
-    Retrieve, update or delete a code api.
+    Retrieve, update or delete a code __.
     """
     try:
         api = Api.objects.get(pk=pk)
@@ -48,5 +50,49 @@ def api_detail(request, pk):
 
     elif request.method == 'DELETE':
         api.delete()
+        return HttpResponse(status=204)
+
+@csrf_exempt
+def all_patient_records(request):
+    """
+    List all patient records or add new record
+    """
+    if request.method == 'GET':
+        record = PatientRecord.objects.all()
+        serializer = RecordsSerializer(api, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = RecordsSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
+@csrf_exempt
+def patient_record(request, pk):
+    """
+    Retrieve, update or delete a patient record.
+    """
+    try:
+        record = PatientRecord.objects.get(pk=pk)
+    except PatientRecord.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        serializer = RecordsSerializer(record)
+        return JsonResponse(serializer.data)
+
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = RecordsSerializer(record, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+
+    elif request.method == 'DELETE':
+        record.delete()
         return HttpResponse(status=204)
 # Create your views here.
