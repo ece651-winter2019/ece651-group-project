6439a3f505d0007fcf8c042b4781f24f7990f16a
Adam Kulas <adam.kulas1@gmail.com>
2019-03-11 16:17:17 -0400

    changed doctors model and user model

--- healthapp_project/healthapp_root/doctors/models.py
+++ healthapp_project/healthapp_root/doctors/models.py
@@ -1,37 +1,16 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 
-class LoginCredentials(models.Model):
-    id = models.CharField(max_length=20, primary_key=True)
-    password = models.CharField(max_length=24)
-
-
-class DoctorLogin(models.Model):
-    login_id = models.AutoField(primary_key=True, null=False)
-    doctor_id = models.ForeignKey("Doctors", on_delete=models.CASCADE, null=False)
-    created_on = models.DateTimeField(default=timezone.now)
-    status = models.IntegerField(null=False)
-
-    # dont know how to add relationship for this
-    # doctor = models.OneToOneField(
-    #     'Doctors',
-    #     on_delete=models.CASCADE,
-    #     parent_link=True
-    #     )
-
-
-class Doctors(models.Model):
-    doctor_id = models.AutoField(primary_key=True)
+class Profiles(models.Model):
+    id = models.AutoField(primary_key=True)
     first_name = models.CharField(max_length=20, null=False)
     last_name = models.CharField(max_length=20, null=False)
-    email = models.CharField(max_length=20, null=False)
-    phone_no = models.IntegerField(null=False)
+    phone_no = models.CharField(max_length=10, null=False)
     street = models.CharField(max_length=20, null=False)
     city = models.CharField(max_length=20, null=False)
-    state = models.CharField(max_length=20, null=False)
+    state = models.CharField(max_length=2, null=False)
     postal_code = models.CharField(max_length=6, null=False)
     country = models.CharField(max_length=20, null=False)
-
-    # patients = [should this be foreign key in patients?]
-    doc_logins = models.OneToOneField(DoctorLogin, on_delete=models.CASCADE)
+    user_id = models.OneToOneField(User, on_delete=models.CASCADE)
--- healthapp_project/healthapp_root/users/admin.py
+++ healthapp_project/healthapp_root/users/admin.py
@@ -4,12 +4,14 @@ from django.contrib.auth.admin import UserAdmin
 
 from .forms import CustomUserCreationForm, CustomUserChangeForm
 from .models import CustomUser
+from doctors.models import Profiles as DocProfiles
 
 
 class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = CustomUser
+    inlines = (DocProfiles,)
     list_display = ["email", "username"]
 
 
