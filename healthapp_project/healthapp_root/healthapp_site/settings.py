c505ca34d2a633a4f04ef8b86fd7f9607d3e6ca5
Ammar Ahmed <ammarahmed@v1020-wn-9-5.campus-dynamic.uwaterloo.ca>
2019-03-12 22:33:46 -0400

    modified settings.py, closes issue #77

--- healthapp_project/healthapp_root/healthapp_site/settings.py
+++ healthapp_project/healthapp_root/healthapp_site/settings.py
@@ -81,8 +81,10 @@ WSGI_APPLICATION = "healthapp_site.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-if 'test' in sys.argv or 'test_coverage' in sys.argv: #Covers regular testing and django-coverage
-    DATABASES = { "default": {"ENGINE": "django.db.backends.sqlite3"} }
+if (
+    "test" in sys.argv or "test_coverage" in sys.argv
+):  # Covers regular testing and django-coverage
+    DATABASES = {"default": {"ENGINE": "django.db.backends.sqlite3"}}
 else:
     DATABASES = {"default": {"ENGINE": "mysql.connector.django"}}
     # Activate Django-Heroku.
@@ -91,7 +93,7 @@ else:
     DATABASES["default"]["ENGINE"] = "mysql.connector.django"
 
 
-#DATABASES = {"default": {"ENGINE": "mysql.connector.django"}}
+# DATABASES = {"default": {"ENGINE": "mysql.connector.django"}}
 
 
 # Password validation
