194305b2a015e1e125645bbf63bd5b2bc11fbcaf
Gabe Richard <gabe_rch@hotmail.com>
2019-03-14 19:03:50 -0400

    Added inline profiles for patients and doctors to admin interface

--- healthapp_project/healthapp_root/users/admin.py
+++ healthapp_project/healthapp_root/users/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 
 from .forms import CustomUserCreationForm, CustomUserChangeForm
 from .models import CustomUser
@@ -8,23 +9,24 @@ from doctors.models import Profile as DocProfile
 from patients.models import Profile as PatProfile
 
 
-# class DocProfileInline(admin.StackedInline):
-#     model = DocProfile
-#     can_delete = False
-#     verbose_name_plural = "doctor profile"
+class DocProfileInline(admin.StackedInline):
+    model = DocProfile
+    can_delete = False
+    verbose_name_plural = "Doctor Profile"
 
 
-# class PatProfileInline(admin.StackedInline):
-#     model = PatProfile
-#     can_delete = False
-#     verbose_name_plural = "patient profile"
+class PatProfileInline(admin.StackedInline):
+    model = PatProfile
+    can_delete = False
+    verbose_name_plural = "Patient Profile"
 
 
 class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = CustomUser
-    # inlines = (DocProfileInline, PatProfileInline)
+    inlines = (DocProfileInline, PatProfileInline)
+
     list_display = [
         "id",
         "email",
@@ -39,6 +41,17 @@ class CustomUserAdmin(UserAdmin):
         "is_doctor",
         "is_patient",
     ]
+
+    def get_inline_instances(self, request, obj=None):
+        if not obj:
+            return list()
+        unfiltered = super(CustomUserAdmin, self).get_inline_instances(request, obj)
+        if obj.is_doctor:
+            return [x for x in unfiltered if isinstance(x, DocProfileInline)]
+        elif obj.is_patient:
+            return [x for x in unfiltered if isinstance(x, PatProfileInline)]
+        else:
+            return unfiltered
 
 
 admin.site.register(CustomUser, CustomUserAdmin)
