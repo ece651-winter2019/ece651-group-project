f7a6ad16161b054e32d5c92eeae8d7b362dee5c2
phoenixtype <43056173+phoenixtype@users.noreply.github.com>
2019-03-15 12:21:27 -0400

    Update tests.py

--- healthapp_project/healthapp_root/api/tests.py
+++ healthapp_project/healthapp_root/api/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
-
+from django.test import SimpleTestCase
 # Create your tests here.
+
+class URLTester(TestCase) :
+    def testingPatientRecord(self):
+        response = self.client.get("patientrecords")
+        self.assertEqual(response.status_code, 200)
+    def testingPatientRecordID(self):
+        pk = input("Hey type a patient key")
+        resp = self.client.get("patientrecords/" + pk)
+        self.assertEqual(resp.status_code, 200)
+    def testingPatientUsername(self):
+        username = input("Type a username")
+        resp = self.client.get("patientrecords/" + username)
+        self.assertEqual(resp.status_code, 200)
+        pass
