4f4910b540f44c960170ab4856cf1c5d6aef1797
Gabe Richard <gabe_rch@hotmail.com>
2019-03-16 20:22:19 -0400

    api tokens

--- healthapp_project/healthapp_root/api/models.py
+++ healthapp_project/healthapp_root/api/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from rest_framework import authentication
+from rest_framework import exceptions
 
-# Create your models here.
+
+# class APIAuthenticate(authentication.TokenAuthentication):
