338d99136c2623e5ee91ff3a4e4f66e9e50487a7
Gabe Richard <gabe_rch@hotmail.com>
2019-03-30 10:23:07 -0400

    small correction

--- healthapp_project/healthapp_root/doctors/tests/test_forms.py
+++ healthapp_project/healthapp_root/doctors/tests/test_forms.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from docotrs.forms import DoctorSignUpForm
+from doctors.forms import DoctorSignUpForm
 from users.models import *
 from django.test import Client
 import datetime
