4e69e5141cfd1ac2810675090096ee855c988248
Ammar Ahmed <ammarahmed@v1020-wn-9-5.campus-dynamic.uwaterloo.ca>
2019-03-30 17:23:29 -0400

    added testcases for views

--- healthapp_project/healthapp_root/users/tests/test_views.py
+++ healthapp_project/healthapp_root/users/tests/test_views.py
@@ -1,9 +1,59 @@
-# from django.test import TestCase
-# import pytest
+from django.test import TestCase
+from django.urls import reverse
+
+from users.models import *
+
+
+class SignUpTest(TestCase):
+
+    def test_view_uses_correct_template(self):
+        response = self.client.get(reverse('signup'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'registration/signup.html')
+
+
+#    def test_Sign_Up_invalid(self):
+#        response = self.client.get('registration/signup.html')
+##        print(response.errors)
+#        self.assertEqual(response.status_code, 200)
+
+
+#class AuthorListViewTest(TestCase):
+#    @classmethod
+#    def setUpTestData(cls):
+#        # Create 13 authors for pagination tests
+#        number_of_authors = 13
 #
+#        for author_id in range(number_of_authors):
+#            Author.objects.create(
+#                                  first_name=f'Christian {author_id}',
+#                                  last_name=f'Surname {author_id}',
+#                                  )
 #
-# def func(x):
-#    return x + 1
+#def test_view_url_exists_at_desired_location(self):
+#    response = self.client.get('/catalog/authors/')
+#    self.assertEqual(response.status_code, 200)
 #
+#    def test_view_url_accessible_by_name(self):
+#        response = self.client.get(reverse('authors'))
+#        self.assertEqual(response.status_code, 200)
 #
-# def test_answer():
+#    def test_view_uses_correct_template(self):
+#        response = self.client.get(reverse('authors'))
+#        self.assertEqual(response.status_code, 200)
+#        self.assertTemplateUsed(response, 'catalog/author_list.html')
+#
+#    def test_pagination_is_ten(self):
+#        response = self.client.get(reverse('authors'))
+#        self.assertEqual(response.status_code, 200)
+#        self.assertTrue('is_paginated' in response.context)
+#        self.assertTrue(response.context['is_paginated'] == True)
+#        self.assertTrue(len(response.context['author_list']) == 10)
+#
+#    def test_lists_all_authors(self):
+#        # Get second page and confirm it has (exactly) remaining 3 items
+#        response = self.client.get(reverse('authors')+'?page=2')
+#        self.assertEqual(response.status_code, 200)
+#        self.assertTrue('is_paginated' in response.context)
+#        self.assertTrue(response.context['is_paginated'] == True)
+#        self.assertTrue(len(response.context['author_list']) == 3)
