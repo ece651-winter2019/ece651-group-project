4f8efb9585b879a774a0d70de9156f269647a376
Adam Kulas <adam.kulas1@gmail.com>
2019-03-31 02:08:07 -0400

    doctor retrieved from db for form test

--- healthapp_project/healthapp_root/patients/tests/test_forms.py
+++ healthapp_project/healthapp_root/patients/tests/test_forms.py
@@ -1,11 +1,11 @@
 from django.test import TestCase
-from patients.forms import *
-from users.models import *
+from patients.forms import PatientSignUpForm
+from users.models import CustomUser
 from django.test import Client
 import datetime
 from django.utils import timezone
-from patients.models import *
-from doctors.models import *
+from patients.models import Profile as PatProfile
+from doctors.models import Profile as DocProfile
 
 
 class Patient_SignUp_Form_Test(TestCase):
@@ -14,65 +14,62 @@ class Patient_SignUp_Form_Test(TestCase):
     #        # Set up non-modified objects used by all test methods
     ##        Profile.objects.create(user_id=1, license_no=123, practice_name="health clinic")
     #        CustomUser.objects.create(is_doctor=True)
-    
+
     # Valid Form Data
     def test_PatientSignUpForm_valid(self):
         form = PatientSignUpForm(
-                                data={
-                                 "doctor_id": 33,
-                                 "dob": "12/12/2000",
-                                 "sex": "Male",
-                                 "contact_firstname": "ammar",
-                                 "contact_lastname": "ahmed",
-                                 "contact_relationship": "Friend",
-                                 "contact_phone":32423442,
-                                "first_name": "ammar",
-                                "last_name": "ahmed",
-                                "phone_no": 23234323,
-                                "street": "uni ave",
-                                "city": "waterloo",
-                                "state": "ON",
-                                "postal_code": "N2L3G5",
-                                "country": "Waterloo",
-                                 "username": "patient",
-                                 "password1": "ECE651project",
-                                 "password2": "ECE651project",
-                                }
-                                )
+            data={
+                "doctor": DocProfile.objects.first().pk,
+                "dob": "12/12/2000",
+                "sex": "Male",
+                "contact_firstname": "ammar",
+                "contact_lastname": "ahmed",
+                "contact_relationship": "Friend",
+                "contact_phone": 32423442,
+                "first_name": "ammar",
+                "last_name": "ahmed",
+                "phone_no": 23234323,
+                "street": "uni ave",
+                "city": "waterloo",
+                "state": "ON",
+                "postal_code": "N2L3G5",
+                "country": "Waterloo",
+                "username": "patient",
+                "password1": "ECE651project",
+                "password2": "ECE651project",
+            }
+        )
         print(form.errors)
         self.assertTrue(form.is_valid())
 
     # Invalid Form Data (signing up without having a doctor id)
     def test_PatientSignUpForm_invalid(self):
         form = PatientSignUpForm(
-                             data={
-                             "doctor_id": "",
-                             "dob": "12/12/2000",
-                             "sex": "Male",
-                             "contact_firstname": "ammar",
-                             "contact_lastname": "ahmed",
-                             "contact_relationship": "Friend",
-                             "contact_phone":32423442,
-                             "first_name": "ammar",
-                             "last_name": "ahmed",
-                             "phone_no": 23234323,
-                             "street": "uni ave",
-                             "city": "waterloo",
-                             "state": "ON",
-                             "postal_code": "N2L3G5",
-                             "country": "Waterloo",
-                             "license_no": 1234,
-                             "practice_name": "health clinic",
-                             "username": "doctor",
-                             "password1": "Ece651proj",
-                             "password2": "Ece651proj",
-                             }
-                             )
+            data={
+                "doctor": "",
+                "dob": "12/12/2000",
+                "sex": "Male",
+                "contact_firstname": "ammar",
+                "contact_lastname": "ahmed",
+                "contact_relationship": "Friend",
+                "contact_phone": 32423442,
+                "first_name": "ammar",
+                "last_name": "ahmed",
+                "phone_no": 23234323,
+                "street": "uni ave",
+                "city": "waterloo",
+                "state": "ON",
+                "postal_code": "N2L3G5",
+                "country": "Waterloo",
+                "license_no": 1234,
+                "practice_name": "health clinic",
+                "username": "doctor",
+                "password1": "Ece651proj",
+                "password2": "Ece651proj",
+            }
+        )
         print(form.errors)
         self.assertFalse(form.is_valid())
-
-
-
 
 
 ##    # Set up non-modified objects used by all test methods
