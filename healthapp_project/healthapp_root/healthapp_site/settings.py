7fb09f7a3e2a5cf7edfedc0ce17e0b44991959be
Adam Kulas <33091324+adkulas@users.noreply.github.com>
2019-10-03 10:16:52 -0400

    Turn off debug mode

--- healthapp_project/healthapp_root/healthapp_site/settings.py
+++ healthapp_project/healthapp_root/healthapp_site/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = "o-u33ekgzqz$cva-bxefke5&kk-%l^c#h#6ukhks+alkwd2m&w"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
